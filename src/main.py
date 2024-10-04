2ec8068b539deb5d74a5dffa68209d4af7104e96
Nate Malin-Jones <nmalin-jones@hotmail.com>
2024-10-03 23:20:41 -0500

    Starting Project

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import json
+import os
+import datetime
+import argparse
+
+
+def main():
+    print("Welcome to your personal task tracker! \n")
+
+    print("")
+
+if __name__ == '__main__':
+    main()
